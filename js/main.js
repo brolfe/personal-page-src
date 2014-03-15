8aaf921d58cb2a93ff739f271af042c052579286
Bryant Rolfe <bryant.rolfe@gmail.com>
2014-03-15 16:58:31 -0400

    Shrinking font size. Container width adjustments for small screens.

--- js/main.js
+++ js/main.js
@@ -3,10 +3,10 @@
     'use strict';
     $(function() {
         $('ul.nav').on( 'click', 'li a', function( ev ) {
+            $('.navbar-collapse').collapse('hide');
+
             // Animate the scroll
             $.scrollTo( ev.target.hash, 250 );
-
-            $('.navbar-collapse').collapse('hide');
         });
 
         $('a.navbar-brand').on( 'click', function() {
@@ -15,13 +15,14 @@
         });
 
         $('#toggle-theme').on( 'click', function() {
+            $('.navbar-collapse').collapse('hide');
+
             var $body = $('body');
             if ( $body.hasClass('dark') ) {
                 $body.removeClass('dark');
             } else {
                 $body.addClass('dark');
             }
-            $('.navbar-collapse').collapse('hide');
         });
     });
 }).call( this, jQuery );
