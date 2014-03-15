d904624d48a36d20630f5d40c9f3a9d9f53c41f4
Bryant Rolfe <bryant.rolfe@gmail.com>
2014-03-15 12:19:18 -0400

    We now have a build and an asset pipeline.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,9 +2,35 @@ module.exports = function(grunt) {
     'use strict';
 
     grunt.loadNpmTasks('grunt-http-server');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-csso');
+    grunt.loadNpmTasks('grunt-usemin');
 
 
     grunt.initConfig({
+
+        jshint: {
+            // Use readJSON rather than the .jshintrc option so that the options
+            // are available for reference in other tasks
+            options: grunt.file.readJSON('.jshintrc'),
+            sourceCode: {
+                src: [ 'js/*.js' ]
+            },
+            grunt: {
+                src: [ 'Gruntfile.js' ],
+                options: {
+                    node: true,
+                    // Relax the number of max statements for the Gruntfile.
+                    maxstatements: 50
+                }
+            }
+        },
+
         'http-server': {
             dev: {
                 root: '.',
@@ -21,9 +47,71 @@ module.exports = function(grunt) {
                 runInBackground: false
             }
         },
+
+        concat: {
+            '.tmp/main-concat.js' : [
+                'js/vendor/bootstrap.js',
+                'js/vendor/jquery.scrollTo.js',
+                'js/main.js'
+            ],
+
+            '.tmp/main-concat.css' : [
+                'css/bootstrap.css',
+                'css/bootstrap-theme.css',
+                '.tmp/main.css'
+            ]
+        },
+
+        less: {
+            '.tmp/main.css' : 'less/main.less'
+        },
+
+        uglify: {
+            'dist/main.min.js' : '.tmp/main-concat.js'
+        },
+
+        csso: {
+            'dist/main.min.css' : '.tmp/main-concat.css'
+        },
+
+        copy: {
+            app: {
+                files: [
+                    {
+                        src: [ 'index.html' ],
+                        dest: 'dist/index.html'
+                    },
+                    {
+                        src: [ 'img/*.jpg' ],
+                        dest: 'dist/'
+                    },
+                    {
+                        src: [ 'js/vendor/modernizr-2.6.2-respond-1.1.0.min.js' ],
+                        dest: 'dist/vendor/modernizr-2.6.2-respond-1.1.0.min.js'
+                    }
+                ]
+            }
+        },
+
+        usemin: {
+            html: ['dist/index.html']
+        },
+
+        clean: [ 'dist', '.tmp' ],
     });
 
     grunt.registerTask('default', [
-        'http-server'
+        // clean
+        'clean',
+        // check files
+        'jshint',
+        // generate files
+        'less',
+        'concat',
+        'copy',
+        // optimize
+        'usemin',
+        'csso',
+        'uglify'
     ]);
 };
--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
-
+/* global jQuery:false */
 (function( $ ) {
+    'use strict';
     $(function() {
         $('ul.nav').on( 'click', 'li a', function( ev ) {
             // Toggle the active class
@@ -10,11 +11,11 @@
             $.scrollTo( ev.target.hash, 250 );
         });
 
-        $('a.navbar-brand').on( 'click', function( ev ) {
+        $('a.navbar-brand').on( 'click', function() {
             // strip active class from nav bar
             $('ul.nav li').removeClass('active');
             // Animate the scroll
-            $("html, body").animate({ scrollTop: 0 }, 250);
-        })
+            $('html, body').animate({ scrollTop: 0 }, 250);
+        });
     });
 }).call( this, jQuery );
